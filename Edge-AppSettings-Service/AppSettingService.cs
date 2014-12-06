2b487461fd4978da9da3e5701e328992ddb9a2ee
Daniel Larsen <daniel@larsen.net.nz>
2014-12-07 08:29:26 +1100

    input to appsetting service is appsetting key

---
+++ Edge-AppSettings-Service/AppSettingService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace EdgeAppSettingsService
+{
+    public class AppSettingService
+    {
+        public async Task<object> Invoke(object input)
+        {
+            string key = (string)input;
+            if (string.IsNullOrEmpty(key)) throw new ArgumentNullException("input");
+
+            string value = System.Configuration.ConfigurationManager.AppSettings[key];
+            if (string.IsNullOrEmpty(value)) throw new InvalidOperationException(key + " is empty or missing.");
+            return value;
+        }
+    }
+}
